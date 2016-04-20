6332fafaf231e1a017dc6677e96b64876c7f9a62
Martin Milev <martinmariusmilev@gmail.com>
2016-04-20 09:08:50 +0300

    Removed commented code.

--- src/ex3/list/array/DemoEx3v1.java
+++ src/ex3/list/array/DemoEx3v1.java
@@ -9,16 +9,16 @@ import ex3.list.exeptions.FullListException;
 public class DemoEx3v1 {
   public static void main(String[] args) throws EmptyListException, FullListException {
     List l1 = new List(4);
-//    l1.add("sasa");
-//    l1.add(123);
-//    l1.add("EWQEW");
-//    l1.add(124);
-//    l1.add(12);
-//    //l1.add(12.4);
-//    System.out.println(l1.print());
-//    l1.remove();
-//    System.out.println(l1.print());
-//    l1.add(124);
+    l1.add("sasa");
+    l1.add(123);
+    l1.add("EWQEW");
+    l1.add(124);
+    l1.add(12);
+    //l1.add(12.4);
+    System.out.println(l1.print());
+    l1.remove();
+    System.out.println(l1.print());
+    l1.add(124);
 //    l1.remove();l1.remove();
 //    System.out.println(l1.print());
 //    l1.remove();l1.remove();
