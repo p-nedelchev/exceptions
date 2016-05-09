f958f2e46bbb2f596dbfe000f7420f065539b148
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-09 14:52:55 +0300

    Rename IntegerToStrignOnlyException to IntegerFormatException

--- ex1/src/IntegerToStringOnlyException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by clouway on 09.05.16.
- */
-public class IntegerToStringOnlyException extends Exception {
-
-        public IntegerToStringOnlyException(){
-            super("Input only integers");
-        }
-}
