8a44288501415d9bbb385786356d24a397ef9e38
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-09 14:48:13 +0300

    Rename IntegerToStringExcetion to IntegerFormatException

--- ex1/src/DemoSummedString.java
+++ ex1/src/DemoSummedString.java
@@ -2,13 +2,13 @@
  * Created by clouway on 09.05.16.
  */
 public class DemoSummedString {
-    public static void main(String[] args) throws IntegerToStringOnlyException {
+    public static void main(String[] args) throws IntegerFormatException {
 
         try {
             SummedString strings = new SummedString();
             System.out.println(strings.sum("9", "-2"));
         } catch (NumberFormatException ex) {
-            throw new IntegerToStringOnlyException();
+            throw new IntegerFormatException();
 
         } catch (Exception e) {
             System.out.println(e);
---
+++ ex1/src/IntegerFormatException.java
@@ -0,0 +1,9 @@
+/**
+ * Created by clouway on 09.05.16.
+ */
+public class IntegerFormatException extends Exception {
+
+        public IntegerFormatException(){
+            super("Input only integers");
+        }
+}
