3012afd140ba2f93f1001d873f2f305ec613cc17
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-10 15:16:53 +0300

    Adding ex3

---
+++ ex3/src/ListIsEmptyException.java
@@ -0,0 +1,8 @@
+/**
+ * Created by clouway on 10.05.16.
+ */
+public class ListIsEmptyException extends Exception {
+    public ListIsEmptyException(){
+        super("Can not remove list is empty");
+    }
+}
---
+++ ex3/src/ListIsFullException.java
@@ -0,0 +1,9 @@
+/**
+ * Created by clouway on 10.05.16.
+ */
+public class ListIsFullException extends Exception{
+
+    public ListIsFullException(){
+        super("Can not add list is full");
+    }
+}
