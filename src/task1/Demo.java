819c8f7821f4071cba49ff32d8ab81a49fd1ff3f
qnislavnachgev <qnislav.nachev@gmail.com>
2016-07-08 16:22:50 +0300

    Done with last changes

--- src/task1/Demo.java
+++ src/task1/Demo.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) throws WrongValue {
+        double result;
         Sumator math = new Sumator();
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter values: ");
-        System.out.println(math.sum(scan.nextLine(), scan.nextLine()));
+        try{
+        result = math.sum(scan.nextLine(), scan.nextLine());
+        }catch (Exception e){
+            throw new WrongValue("This is wrong data");
+        }
+        System.out.println(result);
     }
 }
 
--- src/task1/Sumator.java
+++ src/task1/Sumator.java
@@ -2,13 +2,7 @@ package task1;
 
 public class Sumator {
     public double sum(String a, String b) throws WrongValue {
-        double sum = 0;
-        try {
-            sum = Double.parseDouble(a) + Double.parseDouble(b);
-        } catch (NumberFormatException e) {
-            throw new WrongValue("This is not a correct value");
-        }
-        return sum;
+       return Double.parseDouble(a) + Double.parseDouble(b);
     }
 }
 
