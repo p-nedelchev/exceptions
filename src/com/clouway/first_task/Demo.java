c8f6c14f2ce5033eb6d99ce241bcbf2fb9c1aefc
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-07-18 17:00:22 +0300

    Fist Exception for Sumator

--- src/com/clouway/first_task/Demo.java
+++ src/com/clouway/first_task/Demo.java
@@ -4,4 +4,11 @@ package com.clouway.first_task;
  * @author Petar Nedelchev (peter.krasimirov@gmail.com)
  */
 public class Demo {
+    public static void main(String[] args) {
+
+        Sumator sumator = new Sumator();
+        String a = "25";
+        String b = "15";
+        System.out.println(sumator.sum(a,b));
+    }
 }
--- src/com/clouway/first_task/Sumator.java
+++ src/com/clouway/first_task/Sumator.java
@@ -4,4 +4,21 @@ package com.clouway.first_task;
  * @author Petar Nedelchev (peter.krasimirov@gmail.com)
  */
 public class Sumator {
+
+
+    /**
+     * Parses the content of rwo numeric strings and calculates their sum
+     * @param a String first numeric string
+     * @param b String second numeric string
+     * @return Integer the sum of a and b
+     */
+    public Integer sum(String a, String b) {
+        int sum = 0;
+        try{
+           sum =Integer.parseInt(a) + Integer.parseInt(b);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+        return sum;
+    }
 }
